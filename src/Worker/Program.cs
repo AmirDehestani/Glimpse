ac106a9c1cf7e7a991be6933a9832121d99f640b
Amir Dehestani <amira.dehestani@gmail.com>
2025-12-22 23:42:22 -0500

    Create systemd daemon with install script

--- src/Worker/Program.cs
+++ src/Worker/Program.cs
@@ -1,7 +1,8 @@
 using Worker;
 
 var builder = Host.CreateApplicationBuilder(args);
-builder.Services.AddHostedService<Worker>();
+builder.Services.AddHostedService<GlimpseWorker>();
+builder.Services.AddSystemd();
 
 var host = builder.Build();
 host.Run();
